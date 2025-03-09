3e1bfefce546902ab593fd2f045f77368c52c795
cindyfeldman <cinderella.feldman@gmail.com>
2025-03-09 06:31:26 +0000

    added addtional checks

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -35,7 +35,10 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
 }
 vector<int> extract_shortest_path(const vector<int>&  distances, const vector<int>& previous, int destination)
 {
-    
+    if(destination < 0 || destination >=distances.size()||destination >= previous.size())
+        return {};
+    if(distances[destination] == INF)
+        return {};
     vector<int> path;
     int curr = destination;
     while(curr != -1)
--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main()
 {
     Graph G;
-    file_to_graph("src/small.txt",  G);
+    file_to_graph("src/large.txt",  G);
     vector<int> distance(G.numVertices, -1);
     vector<int> prev(G.numVertices, -1);
     distance = dijkstra_shortest_path(G, 0, prev);
-    vector<int> path = extract_shortest_path(distance, prev,2);
+    vector<int> path = extract_shortest_path(distance, prev,10);
     print_path(path, distance[2]);
     return 0;
 }
